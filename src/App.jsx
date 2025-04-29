09a1bef1c07d0b5459e4799ca3c63aa224f8c095
moham <mohammad9776@gmail.com>
2025-04-29 10:57:39 -0800

    c25-10(setting up r-router and True global style)

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,31 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-//
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      {/* this global style cant accpet any props */}
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <div>
-              <Heading as="h1">Hello World h1</Heading>
-              <Heading as="h2">Hello World h2</Heading>
-              <Heading as="h3">Hello World h3</Heading>
-              {/* we can pass the props in button like regular botton when use styled css */}
-              <Button onClick={() => alert("Check in")}>Check in</Button>
-              <Button
-                varitation="primary"
-                size="small"
-                onClick={() => alert("Check out")}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row type="vertical">
-            <div>
-              <Input type="number" placeholder="number of guest" />
-              <Input type="number" placeholder="number of guest" />
-            </div>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
