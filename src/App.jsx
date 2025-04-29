b09cc5125c77bcc0c96a6976b7d23c79e4c1a847
moham <mohammad9776@gmail.com>
2025-04-29 06:16:28 -0800

    c25-6(Setting up styled-components)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,42 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+//
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
 function App() {
-  return <div>Hello</div>;
+  return (
+    <StyledApp>
+      <H1>Hello World</H1>
+      {/* we can pass the props in button like regular botton when use styled css */}
+      <Button onClick={() => alert("Check in")}>Check in</Button>
+      <Button onClick={() => alert("Check out")}>Check out</Button>
+      <Input type="number" placeholder="number of guest" />
+    </StyledApp>
+  );
 }
 export default App;
