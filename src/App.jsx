7c17a502fef9529a00d1961806ed57d0cc3d36f5
moham <mohammad9776@gmail.com>
2025-04-29 06:46:44 -0800

    c25-7(make style global)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import styled from "styled-components";
-
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
-  cursor: pointer;
-  margin: 20px;
 `;
 
 //
@@ -23,20 +14,19 @@ const StyledApp = styled.div`
   padding: 20px;
 `;
 
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
-`;
 function App() {
   return (
-    <StyledApp>
-      <H1>Hello World</H1>
-      {/* we can pass the props in button like regular botton when use styled css */}
-      <Button onClick={() => alert("Check in")}>Check in</Button>
-      <Button onClick={() => alert("Check out")}>Check out</Button>
-      <Input type="number" placeholder="number of guest" />
-    </StyledApp>
+    <>
+      {/* this global style cant accpet any props */}
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello World</H1>
+        {/* we can pass the props in button like regular botton when use styled css */}
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="number of guest" />
+      </StyledApp>
+    </>
   );
 }
 export default App;
