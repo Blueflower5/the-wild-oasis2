24c864252121f286d85e160f566549e98c652a82
moham <mohammad9776@gmail.com>
2025-04-29 07:35:53 -0800

    c25-9 (more features of styled components)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 //
 const StyledApp = styled.div`
@@ -16,13 +17,31 @@ function App() {
       {/* this global style cant accpet any props */}
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Hello World h1</Heading>
-        <Heading as="h2">Hello World h2</Heading>
-        <Heading as="h3">Hello World h3</Heading>
-        {/* we can pass the props in button like regular botton when use styled css */}
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
-        <Input type="number" placeholder="number of guest" />
+        <Row>
+          <Row type="horizontal">
+            <div>
+              <Heading as="h1">Hello World h1</Heading>
+              <Heading as="h2">Hello World h2</Heading>
+              <Heading as="h3">Hello World h3</Heading>
+              {/* we can pass the props in button like regular botton when use styled css */}
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                varitation="primary"
+                size="small"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <div>
+              <Input type="number" placeholder="number of guest" />
+              <Input type="number" placeholder="number of guest" />
+            </div>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
