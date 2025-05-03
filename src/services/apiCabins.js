ff8daab57eaf3481386d3b865c562df4698bfcf1
Blueflower5 <mohammad9776@gmail.com>
2025-05-03 06:36:15 +0430

    c27-6(how delete item using mutation in reactquery)

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -8,3 +8,11 @@ export async function getCabins() {
   }
   return data;
 }
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.log(error);
+    throw new Error("cabin could not be deleted");
+  }
+  return data;
+}
